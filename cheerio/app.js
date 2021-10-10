d4543b4218c7c39731520a4a59c6f39928c79b88
Savitar <baedsavitar@gmail.com>
2021-10-10 22:27:57 +0200

    test

--- cheerio/app.js
+++ cheerio/app.js
@@ -1,76 +1,70 @@
-const request=require('request')
-const cheerio=require('cheerio')
+const request = require("request");
+const cheerio = require("cheerio");
 
+let t = 1;
 
-//for(let i=0;i<4;i++){
+try {
+  for (i = 0; i < 4; i++) {
+    request(
+      `${process.env.URL}/fr/${process.env.PAGE}=${i}`,
+      async (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+          let $ = await cheerio.load(html);
+          await console.log(i);
+          await $(process.env.SELECTOR).each((i, el) => {
+            const link = $(el).find("a").attr("href");
 
-    let i=0
-   
-request(`${process.env.URL}/fr/${process.env.PAGE}=${i}`,async (error,response,html)=>{
+            try {
+              request(link, async (error, response, html) => {
+                $ = await cheerio.load(html);
 
-  if(!error && response.statusCode==200){
+                const el = await $(".is-sticky");
 
-        let $=await cheerio.load(html)
-     await console.log(i);
-     await $(process.env.SELECTOR).each((i,el)=>{
+                const category = await $(
+                  `#channel-show > div:nth-child(1) > div > nav > ol > li:nth-child(3) > a > span`
+                )
+                  .text()
+                  .trim();
 
-        const link=$(el)
-        .find('a')
-        .attr('href')
+                const tags = await $(`a > span.tag`)
+                  .text()
+                  .trim()
+                  .split("  ")
+                  .join(";");
+                const cName = el.find("a").attr("title").trim();
 
+                const cType = el.find(".card-label").text().trim();
 
-            request(link,async (error,response,html)=>{
+                const cLink = el
+                  .find(".has-text-grey.is-size-7")
+                  .text()
+                  .trim()
+                  .slice(12)
+                  .trim();
 
-            $=await cheerio.load(html)
-            
-            const el=await $('.is-sticky')
+                const cMembers = el.find(".subtitle.is-6").text().trim();
 
+                await console.log({
+                  n: t,
+                  name: cName,
+                  type: cType,
+                  category,
+                  tags,
+                  link: cLink,
+                  nbr: cMembers,
+                });
 
-            const category=await $(`#channel-show > div:nth-child(1) > div > nav > ol > li:nth-child(3) > a > span`)
-                            .text()
-                            .trim()
-
-            const tags=await $(`a > span.tag`)
-                            .text()
-                            .trim()
-                            .split('  ')
-                            .join(';')
-            const cName=el
-                    .find('a')
-                    .attr('title')
-                    .trim()
-
-            const cType=el
-                    .find('.card-label')
-                    .text()
-                    .trim()
-
-             const cLink=el
-                    .find('.has-text-grey.is-size-7')
-                    .text()
-                    .trim()
-                    .slice(12)
-                    .trim()
-            
-            const cMembers=el
-                        .find('.subtitle.is-6')
-                        .text()
-                        .trim()
-
-                    console.log({
-                        name:cName,
-                        type:cType,
-                        category,
-                        tags,
-                        link:cLink,
-                        nbr:cMembers
-                    })
-            })
-     })
-
-    }
-
-
-})
-
+                t++;
+              });
+            } catch (e) {
+              console.log("erreur 1");
+            }
+          });
+        }
+      }
+    );
+  }
+} catch (e) {
+  console.log("erreur 2");
+}
 //}
